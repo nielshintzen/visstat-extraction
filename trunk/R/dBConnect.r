ed74f93b0c3951760ae3a838a005de955b5e5072
doug.beare@gmail.com <doug.beare@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2010-08-04 11:18:56 +0000

    added uid

--- trunk/R/dBConnect.r
+++ trunk/R/dBConnect.r
@@ -2,7 +2,7 @@ dBConnect<-function(which.database="visstat"){
 	library(RODBC)
   odbcCloseAll()
 	if(which.database=="visstat"){
-   conn <- odbcConnect(dsn="visstatp", uid="",pwd="")
+   conn <- odbcConnect(dsn="visstatp", uid="doug",pwd="")
    }
   if(which.database=="frisbe"){
   conn<-odbcConnect(dsn="frisbep",uid="doug",pwd="")
