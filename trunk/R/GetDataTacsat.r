4c7abd25353290eb2a9318723c0270ff1099accb
doug.beare@gmail.com <doug.beare@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2010-08-05 12:21:13 +0000

    Convert raw VMS data to Tacsat format

---
+++ trunk/R/GetDataTacsat.r
@@ -0,0 +1,35 @@
+
+#Cstart <- '15-jan-2010'
+#Cstop  <- '17-jan-2010'
+
+GetDataTacsat <- function(Cstart=Cstart,Cstop=Cstop)
+
+{
+
+print(Cstart)
+
+vms <- GetDataVMS(Cstart=Cstart,Cstop=Cstop)
+print('Got VMS data')
+# Extract the columns
+
+#Reformat time
+
+vms$RGN_UTC_TIME <- ReformatTime(vms$RGN_UTC_TIME)
+vms$RGN_LOCAL_TIME <- ReformatTime(vms$RGN_LOCAL_TIME)
+
+#Tacsat wants a '/' in the data rather than a '-'
+#So reformat date strings
+
+vms$RGN_UTC_DATE <- ReformatDate(vms$RGN_UTC_DATE)
+
+tacsat <- data.frame(VE_REF=vms$TRP_PPY_PLM_CODE,SI_LATI=vms$LATITUDE,SI_LONG=vms$LONGITUDE,
+SI_DATE=vms$RGN_UTC_DATE,SI_TIME=vms$RGN_UTC_TIME,SI_SP = vms$SPEED,SI_HE=vms$HEADING)
+
+tacsat
+
+}
+
+############Example############
+
+
+#  tacsat <- GetDataTacsat(Cstart='15-jan-2010',Cstop='17-jan-2010')
