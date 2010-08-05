cdee4e6888d118ec052683f9f474a5d2cb7fb5eb
doug.beare <doug.beare@gmail.com>
2010-08-05 11:26:12 +0000

    Function to reformat date for eflalo and tacsat formats

---
+++ R/ReFormatDate.r
@@ -0,0 +1,18 @@
+
+
+ReFormatDate<- function(datestring=datestring)
+ {
+#Convert from YYYY-MM-DD
+#datestring <- vms$RGN_UTC_DATE
+datestring <- format(datestring)
+
+
+yr  <-substr(datestring,1,4)
+mon <-substr(datestring,6,7)
+day <-substr(datestring,9,10)
+
+ndate <- paste(day,mon,yr,sep="/")
+
+ndate
+
+}
