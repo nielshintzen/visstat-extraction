37d570eae3d998aa133a71c56fd026bedeb4e352
doug.beare@gmail.com <doug.beare@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2010-08-06 09:48:26 +0000

    anon vessels

--- trunk/R/GetDataTacsat.r
+++ trunk/R/GetDataTacsat.r
@@ -1,6 +1,6 @@
 
-#Cstart <- '15-jan-2010'
-#Cstop  <- '17-jan-2010'
+#Cstart <- '01-jan-2009'
+#Cstop  <- '31-jan-2009'
 
 GetDataTacsat <- function(Cstart=Cstart,Cstop=Cstop)
 
@@ -22,7 +22,7 @@ vms$RGN_LOCAL_TIME <- ReformatTime(vms$RGN_LOCAL_TIME)
 
 vms$RGN_UTC_DATE <- ReformatDate(vms$RGN_UTC_DATE)
 
-tacsat <- data.frame(VE_REF=paste(vms$TRP_PPY_PLM_CODE,vms$VESSEL_ID,sep=":"),SI_LATI=vms$LATITUDE,SI_LONG=vms$LONGITUDE,
+tacsat <- data.frame(VE_REF=paste(vms$TRP_PPY_PLM_CODE,vms$VESSEL_ID1,sep=":"),SI_LATI=vms$LATITUDE,SI_LONG=vms$LONGITUDE,
 SI_DATE=vms$RGN_UTC_DATE,SI_TIME=vms$RGN_UTC_TIME,SI_SP = vms$SPEED,SI_HE=vms$HEADING)
 
 tacsat
@@ -32,4 +32,8 @@ tacsat
 ############Example############
 
 
-#  tacsat <- GetDataTacsat(Cstart='15-jan-2010',Cstop='17-jan-2010')
+#  tacsat <- GetDataTacsat(Cstart='01-jan-2009',Cstop='31-jan-2009')
+
+#   tacsat$VE_REF <- matrix(unlist(strsplit(as.character(tacsat$VE_REF),":")),ncol=2,byrow=T)[,2]
+
+#  write.table (tacsat, file='tacsat.csv',sep=",",row.names=F)
