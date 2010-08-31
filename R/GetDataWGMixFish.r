72f577de920286390f38821c1f3f8938167091b8
doug.beare <doug.beare@gmail.com>
2010-08-31 12:14:04 +0000

    change to the effort table

--- R/GetDataWGMixFish.r
+++ R/GetDataWGMixFish.r
@@ -8,6 +8,7 @@ dats <- data.frame(start.date=paste('1','Jan',syear:eyear,sep="-"), end.date= pa
 #
 for (i in 1:7) {
 
+print(dats$start.date[i])
 data <- GetDataLandingValueByTrip(Cstart=dats$start.date[i],Cstop=dats$end.date[i])
 
 #Just species required
@@ -53,6 +54,7 @@ data$species[ww] <- paste(data$species[ww],as.character(data$NEPFU[ww]),sep="")
 data$narea <- as.character(data$ICES_SUBAREA)
 
 data$narea[data$narea %in% c('IVa','IVb','IVc')] <- "4"
+data$narea[data$narea %in% c('IV')] <- "4"
 data$narea[data$narea %in% 'VIIa'] <- "7a"
 data$narea[data$narea %in% 'VIIc'] <- "7c"
 data$narea[data$narea %in% 'VIIb'] <- "7b"
@@ -84,7 +86,7 @@ tC <- vectorise(tapply(data$WEIGHT,lst,sum.na))
 tV <- vectorise(tapply(data$VALUE,lst,sum.na))
 tC$VALUE<-tV$value
 
-id <- paste(tC$V2,tC$V3,tC$V4,tC$V5,tC$V6,tC$V7,tC$V8,sep='-')
+id <- paste(tC$V2,tC$V3,tC$V4,tC$V5,tC$V6,tC$V7,tC$V8,sep='|')
 
 #Make Table A
 
@@ -94,16 +96,24 @@ SPECIES = tC$V8, LANDINGS = tC$value, DISCARDS=rep(NA,length(tC[,1])), VALUE = t
 
 tabA<-tabA[!is.na(tabA$LANDINGS),]
 
-#Make Table B
+#Make Table B (effort)
 
-lst <- list(data$YEAR,data$QUARTER,data$VESSEL_LENGTH,data$GEAR,data$MESH_SIZE_RANGE,data$narea,data$species)
-tkwd <- vectorise(tapply(data$KWDAS,lst,sum.na))
-tdas <- vectorise(tapply(data$COARSE_DAS,lst,sum.na))
+data1  <- unique(paste(data$YEAR,data$QUARTER,data$VESSEL_LENGTH,data$GEAR,data$MESH_SIZE_RANGE,data$narea,data$COARSE_DAS,data$KWDAS,sep="|"))
+
+
+data1  <- data.frame(matrix(unlist(strsplit(data1,"\\|")),ncol=8,byrow=T))
+data1$DAYS_AT_SEA_EFFORT <- as.numeric(as.vector(data1$X7))
+data1$KW_DAYS_EFFORT <- as.numeric(as.vector(data1$X8))
+
+lst <- list(data1$X1,data1$X2,data1$X3,data1$X4,data1$X5,data1$X6)
+tkwd <- vectorise(tapply(data1$KW_DAYS_EFFORT,lst,sum.na))
+tdas <- vectorise(tapply(data1$DAYS_AT_SEA_EFFORT,lst,sum.na))
 tkwd$das<-tdas$value
 
+id <- paste(tkwd$V2,tkwd$V3,tkwd$V4,tkwd$V5,tkwd$V6,tkwd$V7,sep='|')
+
 tabB <- data.frame(ID=id,
-COUNTRY=rep('NED',length(tkwd[,1])),YEAR =  tkwd$V2, QUARTER= tkwd$V3, VESSEL_LENGTH = tkwd$V4, GEAR = tkwd$V5, MESH_SIZE_RANGE = tkwd$V6, AREA = tkwd$V7,
-SPECIES = tkwd$V8, KW_DAYS_EFFORT = tkwd$value, DAYS_AT_SEA_EFFORT=tkwd$das, NO_VESSELS = '-1')
+COUNTRY=rep('NED',length(tkwd[,1])),YEAR =  tkwd$V2, QUARTER= tkwd$V3, VESSEL_LENGTH = tkwd$V4, GEAR = tkwd$V5, MESH_SIZE_RANGE = tkwd$V6, AREA = tkwd$V7, KW_DAYS_EFFORT = tkwd$value, DAYS_AT_SEA_EFFORT=tkwd$das, NO_VESSELS = '-1')
 
 tabB<-tabB[!is.na(tabB$KW_DAYS_EFFORT),]
 
