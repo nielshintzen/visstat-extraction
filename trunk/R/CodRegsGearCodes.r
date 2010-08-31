5f3fe2f3800437b020abe729e69627b08e3860a5
doug.beare@gmail.com <doug.beare@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2010-08-31 15:00:53 +0000

    This function adds on the gear codes used in the cod management regulations (TR1, TR2, BT1,BT2 etc.)

---
+++ trunk/R/CodRegsGearCodes.r
@@ -0,0 +1,10 @@
+
+CodRegsGearCodes <- function(data=data) {
+#NB run DCFCodes and DCFMeshCategory first
+data$REG_GEAR <- rep(NA,length(data[,1]))
+data$REG_GEAR[data$GEAR == "BEAM" & data$MESH_SIZE_RANGE %in% c(">=120")] <- 'BT1'
+data$REG_GEAR[data$GEAR == "BEAM" & data$MESH_SIZE_RANGE %in% c("80-89","90-99","100-109","100-119")] <- 'BT2'
+data$REG_GEAR[data$GEAR %in% c("OTTER","DEM_SEINE") & data$MESH_SIZE_RANGE %in% c("100-109","100-119",">=120")] <- 'TR1'
+data$REG_GEAR[data$GEAR %in% c("OTTER","DEM_SEINE") & data$MESH_SIZE_RANGE %in% c("80-89","90-99")] <- 'TR2'
+data
+}
