00985c49f10db4df820782bfb12187d6c3bedc4a
niels.hintzen <niels.hintzen@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2010-09-07 15:29:39 +0000

    little change for factor strings to numeric

--- R/FormatTacsat.r
+++ R/FormatTacsat.r
@@ -1,10 +1,10 @@
 formatTacsat <- function(x){
   x$VE_REF    <- ac(x$VE_REF)
-  x$SI_LATI   <- an(x$SI_LATI)
-  x$SI_LONG   <- an(x$SI_LONG)
+  x$SI_LATI   <- an(ac(x$SI_LATI))
+  x$SI_LONG   <- an(ac(x$SI_LONG))
   x$SI_DATE   <- ac(x$SI_DATE)
   x$SI_TIME   <- ac(x$SI_TIME)
-  x$SI_SP     <- an(x$SI_SP)
-  x$SI_HE     <- an(x$SI_HE)
+  x$SI_SP     <- an(ac(x$SI_SP))
+  x$SI_HE     <- an(ac(x$SI_HE))
   return(x)
 }
