90238723eee355e129e572680d55b9987e5388d1
doug.beare <doug.beare@gmail.com>
2010-09-09 14:05:05 +0000

    Make VE_REF numeric

--- R/FormatTacsat.r
+++ R/FormatTacsat.r
@@ -1,5 +1,5 @@
 formatTacsat <- function(x){
-  x$VE_REF    <- ac(x$VE_REF)
+  x$VE_REF    <- an(x$VE_REF)
   x$SI_LATI   <- an(ac(x$SI_LATI))
   x$SI_LONG   <- an(ac(x$SI_LONG))
   x$SI_DATE   <- ac(x$SI_DATE)
