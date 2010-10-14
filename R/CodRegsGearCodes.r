8c80f3c35940ae9d46869fdc73c9839a77f2ce53
doug.beare <doug.beare@gmail.com>
2010-10-14 13:30:50 +0000

    changed input names

--- R/CodRegsGearCodes.r
+++ R/CodRegsGearCodes.r
@@ -1,13 +1,13 @@
 
-CodRegsGearCodes <- function(input=data) {
+CodRegsGearCodes <- function(input=dasbytrip) {
 #NB run DCFCodes and DCFMeshCategory first
-data$REG_GEAR <- rep(NA,length(data[,1]))
-data$REG_GEAR[data$GEAR == "BEAM" & data$MESH_SIZE_RANGE %in% c(">120")] <- 'BT1'
-data$REG_GEAR[data$GEAR == "BEAM" & data$MESH_SIZE_RANGE %in% c("80-89","90-99","100-109","100-119")] <- 'BT2'
-data$REG_GEAR[data$GEAR %in% c("OTTER","DEM_SEINE") & data$MESH_SIZE_RANGE %in% c("100-109","100-119",">=120")] <- 'TR1'
-data$REG_GEAR[data$GEAR %in% c("OTTER","DEM_SEINE") & data$MESH_SIZE_RANGE %in% c("80-89","90-99")] <- 'TR2'
-data$REG_GEAR[data$GEAR %in% c("GILL") ] <- 'GN'
-data$REG_GEAR[data$GEAR %in% c("TRAMMEL") ] <- 'GN'
-data$REG_GEAR[data$GEAR %in% c("LONGLINE") ] <- 'LL'
+input$REG_GEAR <- rep(NA,length(data[,1]))
+input$REG_GEAR[input$GEAR == "BEAM" & input$MESH_SIZE_RANGE %in% c(">120")] <- 'BT1'
+input$REG_GEAR[input$GEAR == "BEAM" & input$MESH_SIZE_RANGE %in% c("80-89","90-99","100-109","100-119")] <- 'BT2'
+input$REG_GEAR[input$GEAR %in% c("OTTER","DEM_SEINE") & input$MESH_SIZE_RANGE %in% c("100-109","100-119",">=120")] <- 'TR1'
+input$REG_GEAR[input$GEAR %in% c("OTTER","DEM_SEINE") & input$MESH_SIZE_RANGE %in% c("80-89","90-99")] <- 'TR2'
+input$REG_GEAR[input$GEAR %in% c("GILL") ] <- 'GN'
+input$REG_GEAR[input$GEAR %in% c("TRAMMEL") ] <- 'GN'
+input$REG_GEAR[input$GEAR %in% c("LONGLINE") ] <- 'LL'
 data
 }
