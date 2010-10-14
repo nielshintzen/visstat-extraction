3f6aa0bac1d891801c0dac1db4beb391ebbcf2c4
doug.beare <doug.beare@gmail.com>
2010-10-14 13:38:30 +0000

    changed name of a variable

--- R/DCFLengthCats.r
+++ R/DCFLengthCats.r
@@ -1,6 +1,6 @@
 
 DCFLengthCats<- function(input=dasbytrip) {
-input$VESSEL_LENGTH <- rep(NA,length(data[,1]))
+input$VESSEL_LENGTH <- rep(NA,length(input[,1]))
 input$VESSEL_LENGTH[input$LENGTH < 10] <- 'u10m'
 input$VESSEL_LENGTH[input$LENGTH > 10 & input$LENGTH <= 15] <-  'o10t15m'
 input$VESSEL_LENGTH[input$LENGTH > 15] <- 'o15m'
