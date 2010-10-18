c549b187ab47e9d0959523f780ab2b1c41a50afa
doug.beare <doug.beare@gmail.com>
2010-10-18 08:51:02 +0000

    give function 2 options, either visstat or eflalo type data names

--- R/DCFLengthCats.r
+++ R/DCFLengthCats.r
@@ -1,8 +1,17 @@
 
-DCFLengthCats<- function(input=dasbytrip) {
+DCFLengthCats<- function(input=dasbytrip,data.type="eflalo") {
 input$VESSEL_LENGTH <- rep(NA,length(input[,1]))
+if(data.type=="eflalo")  {
+input$VESSEL_LENGTH[input$VE_LEN < 10] <- 'u10m'
+input$VESSEL_LENGTH[input$VE_LEN >= 10 & input$VE_LEN < 15] <-  'o10t15m'
+input$VESSEL_LENGTH[input$VE_LEN >= 15] <- 'o15m'  }
+else{
 input$VESSEL_LENGTH[input$LENGTH < 10] <- 'u10m'
-input$VESSEL_LENGTH[input$LENGTH > 10 & input$LENGTH <= 15] <-  'o10t15m'
-input$VESSEL_LENGTH[input$LENGTH > 15] <- 'o15m'
+input$VESSEL_LENGTH[input$LENGTH >= 10 & input$VE_LEN < 15] <-  'o10t15m'
+input$VESSEL_LENGTH[input$LENGTH >= 15] <- 'o15m'  }
+
+
+
+
 input
 }
