dad9b8a04b323d4626d7a36adc145514041d9567
doug.beare@gmail.com <doug.beare@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2010-11-11 14:10:49 +0000

    Added a comment

--- trunk/R/ReFormatDate.r
+++ trunk/R/ReFormatDate.r
@@ -3,6 +3,7 @@
 ReformatDate<- function(datestring=datestring)
  {
 #Convert from YYYY-MM-DD
+#This when preparing eflalo and tacsat data
 #datestring <- vms$RGN_UTC_DATE
 datestring <- format(datestring)
 
