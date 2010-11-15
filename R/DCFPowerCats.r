a1edfad7ecfe478938801d8a3f5a010e9d208f23
doug.beare <doug.beare@gmail.com>
2010-11-15 10:57:17 +0000

    create power categories

---
+++ R/DCFPowerCats.r
@@ -0,0 +1,22 @@
+
+DCFPowerCats<- function(input=dasbytrip,data.type="eflalo") {
+
+input$kW_cat <- rep(NA,length(input[,1]))
+
+if(data.type=="eflalo")  {
+input$kW_cat[input$VE_KW <= 221] <- 'lt221kW'
+input$kW_cat[input$VE_KW > 221] <- 'gt221kW'
+ }
+
+else {
+input$kW_cat[input$POWER <= 221] <- 'lt221kW'
+input$kW_cat[input$POWER >  221] <- 'gt221kW'
+ }
+
+
+
+
+
+
+input
+}
