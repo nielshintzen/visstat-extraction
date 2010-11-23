dfcd55e617f55704ef55966c305672b0249b0dbe
doug.beare <doug.beare@gmail.com>
2010-11-23 10:27:18 +0000

    Prints top 5 rows of tables in VISSTAT.

---
+++ R/GetInfoVisstat.r
@@ -0,0 +1,51 @@
+
+ 
+GetInfoVisstat <- function() 
+{
+ 
+visstat <- dBConnect(which.database="visstat") 
+
+#Get CATCHES  
+
+qca <- paste("select * FROM CATCHES WHERE rownum < 5")
+ 
+ print(sqlQuery(visstat,qca))  
+
+  print("CATCHES")
+ 
+ # Get GEAR PROPERTIES
+  
+qgp <- paste("select * FROM GEAR_PROPERTIES WHERE rownum < 5")
+ 
+ print(sqlQuery(visstat,qgp))    
+
+ print("GEAR PROPERTIES")
+ 
+ #Get PLATFORM PROPERTIES
+
+qpp <- paste("select * FROM PLATFORM_PROPERTIES WHERE rownum < 5")
+ 
+ print(sqlQuery(visstat,qpp))    
+
+ print("PLATFORM PROPERTIES")
+
+#Get REGISTRATIONS
+
+qre <- paste("select * FROM REGISTRATIONS WHERE rownum < 5")
+ 
+ print(sqlQuery(visstat,qre))  
+ 
+ print("REGISTRATIONS")  
+
+ #Get TRIPS
+
+qtr <- paste("select * FROM TRIPS WHERE rownum < 5")
+ 
+ print(sqlQuery(visstat,qtr))  
+ 
+ print("TRIPS")  
+
+  
+} 
+  
+  
