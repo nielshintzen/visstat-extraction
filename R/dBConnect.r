5d4486587e3155c7e9d9e9b9aa9bb306c571279e
doug.beare <doug.beare@gmail.com>
2010-12-10 15:55:25 +0000

    Add switch for whichever RODBC or DBI

--- R/dBConnect.r
+++ R/dBConnect.r
@@ -1,13 +1,28 @@
-dBConnect<-function(which.database="visstat"){
-	library(RODBC)
+dBConnect<-function(which.lib = "RODBC", which.database="visstat"){
+	
+  #Options for libraries are RODBC and DBI (DBI is set up to work on raja and RODBC usually on a windoze box
+  if(which.lib=="RODBC"){
+  library(RODBC)
   odbcCloseAll()
+  
 	if(which.database=="visstat"){
    conn <- odbcConnect(dsn="visstatp", uid="doug",pwd="oneover")
    }
   if(which.database=="frisbe"){
   conn<-odbcConnect(dsn="frisbep",uid="doug",pwd="ninethirty")
   }
+  }
+  if(which.lib=="DBI"){
+  library(DBI);library(ROracle)
+  if(which.database=="visstat"){
+  drv <- dbDriver("Oracle")
+  conn<-dbConnect(drv,user="doug",password="oneover",db="visstatp")
+  }
+  if(which.database=="frisbe"){
+  conn<-dbConnect(drv,user="doug",password="ninethirty",db="frisbep")
+  }
+  }
 	conn
 }
 
- 
+                                                            
