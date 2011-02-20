7dfd1b0fa57bd0f4fd273c382f5bd1559a7472ac
doug.beare@gmail.com <doug.beare@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2011-02-20 11:46:41 +0000

    Added the distinction for TR1.1 and TR1.2

--- trunk/R/CodRegsGearCodes.r
+++ trunk/R/CodRegsGearCodes.r
@@ -2,12 +2,17 @@
 CodRegsGearCodes <- function(input=dasbytrip) {
 #NB run DCFCodes and DCFMeshCategory first
 input$REG_GEAR <- rep(NA,length(input[,1]))
+
 input$REG_GEAR[input$GEAR == "BEAM" & input$MESH_SIZE_RANGE %in% c(">120")] <- 'BT1'
 input$REG_GEAR[input$GEAR == "BEAM" & input$MESH_SIZE_RANGE %in% c("80-89","90-99","100-109","100-119")] <- 'BT2'
-input$REG_GEAR[input$GEAR %in% c("OTTER","DEM_SEINE") & input$MESH_SIZE_RANGE %in% c("100-109","100-119",">=120")] <- 'TR1'
+
+input$REG_GEAR[input$GEAR %in% c("OTTER","DEM_SEINE") & input$MESH_SIZE_RANGE %in% c("100-109","100-119")] <- 'TR1.1'
+input$REG_GEAR[input$GEAR %in% c("OTTER","DEM_SEINE") & input$MESH_SIZE_RANGE %in% c(">=120")] <- 'TR1.2'
+
 input$REG_GEAR[input$GEAR %in% c("OTTER","DEM_SEINE") & input$MESH_SIZE_RANGE %in% c("80-89","90-99")] <- 'TR2'
 input$REG_GEAR[input$GEAR %in% c("GILL") ] <- 'GN'
 input$REG_GEAR[input$GEAR %in% c("TRAMMEL") ] <- 'GN'
 input$REG_GEAR[input$GEAR %in% c("LONGLINE") ] <- 'LL'
+
 input
 }
