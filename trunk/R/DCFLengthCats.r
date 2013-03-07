c07590ce81fa2240ebc9bc983616d45b83cab9a2
marcel.machiels@gmail.com <marcel.machiels@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2013-03-07 10:24:25 +0000

    corr line 9

--- trunk/R/DCFLengthCats.r
+++ trunk/R/DCFLengthCats.r
@@ -1,4 +1,3 @@
-
 DCFLengthCats<- function(input=dasbytrip,data.type="eflalo") {
 input$VESSEL_LENGTH <- rep(NA,length(input[,1]))
 if(data.type=="eflalo")  {
@@ -7,7 +6,7 @@ input$VESSEL_LENGTH[input$VE_LEN >= 10 & input$VE_LEN < 15] <-  'o10t15m'
 input$VESSEL_LENGTH[input$VE_LEN >= 15] <- 'o15m'  }
 else{
 input$VESSEL_LENGTH[input$LENGTH < 10] <- 'u10m'
-input$VESSEL_LENGTH[input$LENGTH >= 10 & input$VE_LEN < 15] <-  'o10t15m'
+input$VESSEL_LENGTH[input$LENGTH >= 10 & input$LENGTH < 15] <-  'o10t15m'
 input$VESSEL_LENGTH[input$LENGTH >= 15] <- 'o15m'  }
 
 
