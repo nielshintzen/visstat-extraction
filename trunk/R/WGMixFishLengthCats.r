4b81444852f8112e21daaffd0f64353b916b3c03
marcel.machiels@gmail.com <marcel.machiels@gmail.com@de15ecf6-407a-873d-d5f4-00b220b28dd3>
2013-03-27 12:37:42 +0000

    new length categories

--- trunk/R/WGMixFishLengthCats.r
+++ trunk/R/WGMixFishLengthCats.r
@@ -1,10 +1,10 @@
-
-
 WGMixFishLengthCats <- function(data=data) {
 data$VESSEL_LENGTH <- rep(NA,length(data[,1]))
-data$VESSEL_LENGTH[data$LENGTH < 12] <- 'u12m'
-data$VESSEL_LENGTH[data$LENGTH >= 12 & data$LENGTH < 24] <-  'o12t24m'
-data$VESSEL_LENGTH[data$LENGTH >= 24 & data$LENGTH < 40] <-  'o24t40m'
-data$VESSEL_LENGTH[data$LENGTH >= 40] <- 'o40m'
+data$VESSEL_LENGTH[data$LENGTH < 10] <- '<10'
+data$VESSEL_LENGTH[data$LENGTH >= 10 & data$LENGTH < 12] <-  '10<12'
+data$VESSEL_LENGTH[data$LENGTH >= 12 & data$LENGTH < 18] <-  '12<18'
+data$VESSEL_LENGTH[data$LENGTH >= 18 & data$LENGTH < 24] <-  '18<24'
+data$VESSEL_LENGTH[data$LENGTH >= 24 & data$LENGTH < 40] <-  '24<40'
+data$VESSEL_LENGTH[data$LENGTH >= 40] <- '>=40'
 data
 }
